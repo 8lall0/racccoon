53112a2c8c406426cdb304c749c2afad792c9e78
8lall0 <savino.liguori@gmail.com>
2025-12-11 01:21:52 +0100

    Working boot, std::io, panic handler.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 rm -rf ../build/obj
+rm -rf ../build/kernel.*
 
 set -e
 
@@ -8,17 +9,8 @@ set -e
   cd ..
   c3c build --no-entry --safe=no --riscv-cpu=rvimac
 
-  OBJS=`ls build/obj/elf-riscv32/*.o`
-  DEL="build/obj/elf-riscv32/racccoon.o"
-  for del in ${DEL[@]}
-  do
-     OBJS=("${OBJS[@]/$del}") #Quotes when working with strings
-  done
-
-  /opt/riscv/bin/llvm-ar rcs build/obj/elf-riscv32/libs.a $OBJS
   /opt/riscv/bin/ld.lld \
-    build/obj/elf-riscv32/racccoon.o \
-    build/obj/elf-riscv32/libs.a \
+    build/obj/elf-riscv32/*.o \
     -T src/kernel.ld \
     -L /opt/riscv/lib/linux \
     -lclang_rt.builtins-riscv32 \
