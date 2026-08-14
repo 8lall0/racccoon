09e617ec1744e79808080975217f567c21f235a9
8lall0 <savino.liguori@gmail.com>
2026-08-14 17:14:24 +0200

    Ch 15 from the RISC-V os in 1000 lines done

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -8,4 +8,6 @@ qemu-system-riscv32 \
   -nographic \
   -serial mon:stdio \
   -no-reboot \
+  -drive id=drive0,file=../resources/disk.txt,format=raw,if=none \
+  -device virtio-blk-device,drive=drive0,bus=virtio-mmio-bus.0 \
   -kernel ../build/kernel.elf
